b0b1fd44ea203a2bfbac02e79a458455b18bee0f
s-amano <amahaya0831@gmail.com>
2022-02-14 13:29:51 +0900

    fix: ReduxDevToolsを利用できるようにstoreを修正

--- src/reducks/store/store.ts
+++ src/reducks/store/store.ts
@@ -1,4 +1,4 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 
 import { persistReducer, persistStore } from "redux-persist";
 import storage from "redux-persist/lib/storage";
@@ -10,6 +10,13 @@ import { MessagesReducer } from "reducks/messages/reducers";
 
 import { User } from "reducks/user/types";
 import { Messages } from "reducks/messages/types";
+
+// windowが__REDUX_DEVTOOLS_EXTENSION_COMPOSE__を持つこととその型を明示する必要がある
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+  }
+}
 
 export interface RootState {
   user: User;
@@ -32,7 +39,13 @@ const persistedReducer = persistReducer(
   })
 );
 
-const store = createStore(persistedReducer, applyMiddleware(thunk));
+// ReduxDevToolsを利用するための設定
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  persistedReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
 
 export const persistor = persistStore(store);
 export default store;
